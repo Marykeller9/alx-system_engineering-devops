e90ef974883c8ad8aa4a801718e878727bb368ec
Marykeller9 <marykeller9@gmail.com>
2023-09-24 06:22:48 -0700

    config finale

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 #This code kills a process & and works together with the killmenow file which has already been provided
-exec { killmenow':
-  command  =>  '/usr//bin/pkill killmenow',
-  provider =>  'shell'
-  return   =>  [0, 1],
+exec { pkill:
+  command  =>  'pkill killmenow',
+  provider =>  'shell',
 }
