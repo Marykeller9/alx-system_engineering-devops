5b21ffa8b087ea1166d6cc6c20eef56214fc936f
Marykeller9 <marykeller9@gmail.com>
2023-09-24 06:32:05 -0700

    create a file config

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,5 @@
 #The code will create a file name schol inside the /tmp Direstory
 file { '/tmp/school':
-  ensure    =>  file,
   content   =>  'I love Puppet',
   mode      =>  '0744',
   owner     =>  'www-data',
